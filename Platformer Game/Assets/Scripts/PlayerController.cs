501d27969c875510decb5a97475ee159ec4646b2
Ducky <masterless123@gmail.com>
2021-12-02 09:28:19 -0500

    Week 3 (Animations and Tilesets)

--- Platformer Game/Assets/Scripts/PlayerController.cs
+++ Platformer Game/Assets/Scripts/PlayerController.cs
@@ -7,12 +7,14 @@ public class PlayerController : MonoBehaviour
     public float playerVelocity = 3.0f;
     public LayerMask groundLayer;
     public float jumpPower = 5f; 
-    Rigidbody2D rigidbody2D; 
+    Rigidbody2D rigidbody2D;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,11 +26,21 @@ public class PlayerController : MonoBehaviour
         
         if (moveX > 0) { // if player wants move right
             transform.localScale = new Vector3(1, 1, 1);
+            anim.SetBool("Is Moving", true);
         } else if (moveX < 0) { // if player wants move left
             transform.localScale = new Vector3(-1, 1, 1);
+            anim.SetBool("Is Moving", true);
+        } else {
+            anim.SetBool("Is Moving", false);
         }
 
         Jump();
+
+        if (rigidbody2D.velocity.y < 0) {
+            anim.SetBool("Is Falling", true);
+        } else {
+            anim.SetBool("Is Falling", false);
+        }
     }
 
     private bool touchingGround;
@@ -44,10 +56,12 @@ public class PlayerController : MonoBehaviour
         
         bool jumpInput = Input.GetKeyDown(KeyCode.Space);
         if (jumpInput) {
+
             if (touchingGround || !hasDoubleJumped) {
                 // rigidbody2D.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
                 rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpPower);
                 hasDoubleJumped = true;
+                anim.SetTrigger("Has Jumped");
             }
         }
 
