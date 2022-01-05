03dc5b09c565975473f77dc3b42da51d4684a39f
Eric <31218485+16BitNarwhal@users.noreply.github.com>
2022-01-05 14:36:32 -0500

    UI

---
+++ Platformer Game/Assets/LoadScene.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    
+    public void Load(string sceneName) {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+
+    }
+
+}
